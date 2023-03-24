4ff824c460083ca95461e09fff55c5dd54b9f195
heli-ih <86485334+heli-ih@users.noreply.github.com>
2023-03-24 20:32:09 +0400

    first commit

---
+++ tailwind-cli/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./main/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#2E2E46',
+        light: '#3A3956',
+        violet: '#9999C2',
+        menu: '#626284',
+        menulight: '#EB638B',
+        white: '#FBFBFF',
+        card: '#EA6CA1',
+      },
+      height: {
+        '500': '700px',
+      }
+    },
+  },
+  plugins: [],
+}
